28490cba82a2b265324f9ea1f009585cc79b97f9
Fernando 'Brujo' Benavides <greenmellon@gmail.com>
2010-01-19 15:21:02 -0300

    First version - nothing to see yet

---
+++ src/Main.hs
@@ -0,0 +1,40 @@
+-- | This module provides the application main "window" which is no real window
+--   at all.  It just holds the menus so you can open each step of the tutorial
+--   or close it all at the same time.
+module Main where
+
+import Graphics.UI.WX
+import Graphics.UI.WXCore
+
+-- | The application entry point
+main :: IO ()
+main = start gui
+
+gui :: IO ()
+gui =
+    do
+        -- We create a hidden window
+        win <- frame [text := "wxHNotepad", visible := False]
+
+        -- If this window is closed, the application ends
+        set win [on closing := wxcAppExit]
+        
+        let say title desc = infoDialog win title desc
+        
+        -- The only thing that this window will have is its menu bar to open all
+        -- the other windows
+        mnuSteps <- menuPane [text := "Steps"]
+        menuItem mnuSteps [text := "Step &1\tCtrl-1", on command := say "Step 1" "Just a Text Field"]
+        menuItem mnuSteps [text := "Step &2\tCtrl-2", on command := say "Step 2" "Undo / Redo"]
+        menuItem mnuSteps [text := "Step &3\tCtrl-3", on command := say "Step 3" "Cut / Copy / Paste"]
+        menuItem mnuSteps [text := "Step &4\tCtrl-4", on command := say "Step 4" "Open / Save / Save As..."]
+        menuItem mnuSteps [text := "Step &5\tCtrl-5", on command := say "Step 5" "Toolbar with Images"]
+        menuItem mnuSteps [text := "Step &6\tCtrl-6", on command := say "Step 6" "Preferences..."]
+        menuQuit mnuSteps [on command := wxcAppExit]
+
+        -- the simplest about page...
+        mnuHelp <- menuHelp []
+        menuAbout mnuHelp [on command := say "About wxHNotepad" "Author: Fernando Brujo Benavides\nWebsite: http://github.com/elbrujohalcon/wxhnotepad"]
+        
+        -- and... RUN, FORREST!!
+        set win [menuBar := [mnuSteps, mnuHelp]]
